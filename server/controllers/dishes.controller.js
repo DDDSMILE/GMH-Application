b9b7cd9d05eb7a0948a321734b578a6df241d1e5
thairyo <sonthai1310.works@gmail.com>
2023-04-20 19:14:48 +0700

    req params dishes.

--- server/controllers/dishes.controller.js
+++ server/controllers/dishes.controller.js
@@ -2,11 +2,33 @@ import { DishesModel } from "../models/dishes.model.js";
 
 export const getDishesWithPaginate = async (req, res) => {
   try {
-    const page = req.params;
+    const page = req.params.page || 1;
+    const productType = decodeURIComponent(req.params.type);
     const pageSize = 10;
     const offset = (page - 1) * pageSize;
+    const minPrice = req.params.min || 0;
+    const maxPrice = req.params.max || Infinity;
+    const sortDishes = req.params.sort || "asc";
 
-    const dishes = await DishesModel.find().limit(pageSize).skip(offset);
+    const sortParam = sortDishes === "asc" ? { price: 1 } : { price: -1 };
+    let dishes = [];
+    if (productType === "all") {
+      dishes = await DishesModel.find({
+        price: { $gte: minPrice, $lte: maxPrice },
+      })
+        .sort(sortParam)
+        .limit(pageSize)
+        .skip(offset);
+    } else {
+      dishes = await DishesModel.find({
+        type: productType,
+        price: { $gte: minPrice, $lte: maxPrice },
+      })
+        .sort(sortParam)
+        .limit(pageSize)
+        .skip(offset);
+    }
+
     res.status(200).json({ success: true, message: "Done", dishes: dishes });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
--- server/routes/dishes.route.js
+++ server/routes/dishes.route.js
@@ -3,6 +3,7 @@ import { getDishesWithPaginate } from "../controllers/dishes.controller.js";
 
 const router = express.Router();
 
-router.get("/:page", getDishesWithPaginate);
+// http://www.localhost:3001/api/v1/dishes/all/2/5000&25000/sort=
+router.get("/:type?/:page?/:min?&:max?/:sort=?", getDishesWithPaginate);
 
 export default router;
