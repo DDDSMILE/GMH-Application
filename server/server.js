82926b1d502ba30da28dd021bc8f5c173aee8a66
thairyo <sonthai1310.works@gmail.com>
2023-05-27 22:50:34 +0700

    shorted distance by haversine.

--- server/server.js
+++ server/server.js
@@ -184,6 +184,135 @@ const backupCollections = async () => {
   }
 };
 
+const shortedDistance = () => {
+  const address = [
+    {
+      person_a: {
+        name: "person_a",
+        lat: 16.070219226894224,
+        long: 108.21175224334318,
+      },
+    },
+    {
+      supplier: [
+        { name: "b", lat: 16.06635934370604, long: 108.19149809971044 },
+        { name: "c", lat: 16.061203059711115, long: 108.1759485105243 },
+        { name: "a", lat: 16.052001221989833, long: 108.2102114880597 },
+      ],
+    },
+    {
+      person_b: {
+        name: "person_b",
+        lat: 16.08370380445022,
+        long: 108.22860544450404,
+      },
+    },
+  ];
+
+  // Tính khoảng cách giữa hai điểm sử dụng công thức Haversine
+  function calculateDistance(lat1, lon1, lat2, lon2) {
+    const earthRadius = 6371; // Đường kính trái đất (đơn vị: km)
+
+    const dLat = toRad(lat2 - lat1);
+    const dLon = toRad(lon2 - lon1);
+
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(toRad(lat1)) *
+        Math.cos(toRad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const distance = earthRadius * c;
+    return distance;
+  }
+
+  // Chuyển đổi từ độ sang radian
+  function toRad(value) {
+    return (value * Math.PI) / 180;
+  }
+
+  const personA = address[0].person_a;
+  const suppliers = address[1].supplier;
+  const personB = address[2].person_b;
+
+  const speed = 40; // Vận tốc (đơn vị: km/h)
+
+  // 1. Tính quãng đường ngắn nhất từ person_a đến person_b
+  const distancePersonAToPersonB = calculateDistance(
+    personA.lat,
+    personA.long,
+    personB.lat,
+    personB.long
+  );
+
+  // 2. Tính quãng đường ngắn nhất từ person_a đến các điểm a, b, c và thời gian đi
+  const distances = [];
+  const times = [];
+
+  suppliers.forEach((supplier) => {
+    const distance = calculateDistance(
+      personA.lat,
+      personA.long,
+      supplier.lat,
+      supplier.long
+    );
+    const time = distance / speed;
+    distances.push(distance);
+    times.push(time);
+  });
+
+  // Sắp xếp theo thứ tự tăng dần của quãng đường
+  const sortedIndices = distances
+    .map((_, index) => index)
+    .sort((a, b) => distances[a] - distances[b]);
+  const sortedDistances = sortedIndices.map((index) => distances[index]);
+  const sortedTimes = sortedIndices.map((index) => times[index]);
+  const sortedSuppliers = sortedIndices.map((index) => suppliers[index]);
+
+  // 3. Xuất ra quãng đường từ person_a đến a, a đến b, b đến c, c đến person_b
+  console.log("Kết quả:");
+  console.log("-------");
+
+  // Quãng đường từ person_a đến person_b
+  console.log(
+    `- person_a đến person_b: ${distancePersonAToPersonB} km, Thời gian: ${
+      (distancePersonAToPersonB / speed) * 60
+    } phút`
+  );
+
+  // Quãng đường từ person_a đến các điểm a, b, c và thời gian đi
+  let previousPoint = personA;
+  sortedSuppliers.forEach((supplier, index) => {
+    const distance = sortedDistances[index];
+    const time = sortedTimes[index];
+    console.log(
+      `- ${previousPoint.name} đến ${
+        supplier.name
+      }: ${distance} km, Thời gian: ${time * 60} phút`
+    );
+    previousPoint = supplier;
+  });
+  // Quãng đường từ c đến person_b
+  const distanceCToPersonB = calculateDistance(
+    previousPoint.lat,
+    previousPoint.long,
+    personB.lat,
+    personB.long
+  );
+  console.log(
+    `- ${
+      previousPoint.name
+    } đến person_b: ${distanceCToPersonB} km, Thời gian: ${
+      (distanceCToPersonB / speed) * 60
+    } phút`
+  );
+};
+
+shortedDistance();
+
 cron.schedule("0 1 * * *", () => {
   console.log("Start crape data at 1:00 AM");
   // updatedDishes();
